d7ab155618d7c0d9352e3f83b8c5751628596f87
Guillermo Cervera <g.cervera10@gmail.com>
2021-03-27 10:15:51 +0100

    modified game.py and added hero.py

--- shmup/game.py
+++ shmup/game.py
@@ -5,6 +5,7 @@ import os
 
 from shmup.fpsstats import FPSStats
 from shmup.config import Config
+from shmup.hero import Hero
 
 class Game:
 
@@ -14,18 +15,12 @@ class Game:
         self.__screen = pygame.display.set_mode(Config.screen_size,0,32)
         pygame.display.set_caption(Config.game_title)
 
-        self.__hero = pygame.image.load(os.path.join(*Config.hero_filename)).convert_alpha()
-        self.__running = False
+        self.__hero = Hero(self.__screen.get_size())
 
-        self.__is_moving_up = False
-        self.__is_moving_right = False
-        self.__is_moving_down = False
-        self.__is_moving_left = False
-
-        self.__hero_position = pygame.math.Vector2(self.__screen.get_width()/2 - self.__hero.get_width()/2, self.__screen.get_height() - self.__hero.get_height())        
         self.__font = pygame.font.Font(os.path.join(*Config.font_filename), Config.font_fps_size)
-
         self.__fps_stats = FPSStats(self.__font)
+
+        self.__running = False
 
     def run(self):       
         self.__running = True
@@ -43,7 +38,7 @@ class Game:
                 self.__update(Config.time_per_frame)
                 self.__fps_stats.update_logic(Config.time_per_frame)
 
-            self.__render()   
+            self.__render()
             self.__fps_stats.update_render()         
         
         self.__quit()        
@@ -54,46 +49,22 @@ class Game:
                 self.__running = False
 
             if event.type == pygame.KEYDOWN:
-                self.__player_controller(True, event.key)
+                self.__hero.handle_input(True, event.key)
             if event.type == pygame.KEYUP:
-                self.__player_controller(False, event.key)
-
+                self.__hero.handle_input(False, event.key)
 
     def __update(self, delta):
-        move = pygame.math.Vector2(0.0, 0.0)
-
-        if self.__is_moving_up:
-            move.y -= Config.hero_speed
-        if self.__is_moving_right:
-            move.x += Config.hero_speed
-        if self.__is_moving_down:
-            move.y += Config.hero_speed
-        if self.__is_moving_left:
-            move.x -= Config.hero_speed
-
-        self.__hero_position += move * delta
+        self.__hero.update(delta)
 
     def __render(self):
         self.__screen.fill(Config.background_color)
-
-        self.__screen.blit(self.__hero, self.__hero_position.xy)
-
+        self.__hero.render(self.__screen)
         self.__fps_stats.render_stats(self.__screen)
-
         pygame.display.update()
 
     def __quit(self):
+        self.__hero.release()
         pygame.quit()
-
-    def __player_controller(self, pressed, key):
-        if key == pygame.K_UP:
-            self.__is_moving_up = pressed
-        elif key == pygame.K_RIGHT:
-            self.__is_moving_right = pressed
-        elif key == pygame.K_DOWN:
-            self.__is_moving_down = pressed
-        elif key == pygame.K_LEFT:
-            self.__is_moving_left = pressed
 
     def __calc_delta(self, last):
         current = pygame.time.get_ticks()
